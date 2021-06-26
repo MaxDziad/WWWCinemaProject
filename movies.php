c3fbab9cec9337215f266613325438da156f2c94
Maximiriam <dziadon.kakuzu@gmail.com>
2021-06-26 22:09:27 +0200

    repertuar update

--- movies.php
+++ movies.php
@@ -9,6 +9,9 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     $title = htmlspecialchars($row['title'], ENT_QUOTES | ENT_HTML401);
     $short_description = htmlspecialchars($row['short_description'], ENT_QUOTES | ENT_HTML401);
     $genre = htmlspecialchars($row['genre'], ENT_QUOTES | ENT_HTML401);
+    $duration = intval($row['duration']);
+    $production = htmlspecialchars($row['production'], ENT_QUOTES | ENT_HTML401);
+    $direction = htmlspecialchars($row['direction'], ENT_QUOTES | ENT_HTML401);
     $poster_path = htmlspecialchars($row['poster_path'], ENT_QUOTES | ENT_HTML401);
     $available_date = $row['available_date'];
 
@@ -16,6 +19,9 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         'title' => $title,
         'short_description' => $short_description,
         'genre' => $genre,
+        'duration' => $duration,
+        'production' => $production,
+        'direction' => $direction,
         'poster_path' => $poster_path,
         'available_date' => $available_date
     ]);
