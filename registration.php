0e17980bc81b6d26d9c9e551b4c023ecc6f97342
agunia2000 <gunia.amadeusz@gmaill.com>
2021-06-28 20:20:42 +0200

    rejestracja update

--- registration.php
+++ registration.php
@@ -2,11 +2,15 @@
 
 $komunikat='';
 
-if (isset($_POST['new_name']) && isset($_POST['new_surname']) && isset($_POST['new_email']) && isset($_POST['new_phonenumber']) && isset($_POST['new_password']) && isset($_POST['new_password_confirm']) ) {
+if (isset($_POST['new_name']) && isset($_POST['new_surname']) && isset($_POST['new_address']) && isset($_POST['new_address_cd']) && isset($_POST['new_postcode']) && isset($_POST['new_city']) && isset($_POST['new_email']) && isset($_POST['new_phone_number']) && isset($_POST['new_password']) && isset($_POST['new_password_confirm']) ) {
     $new_name = $_POST['new_name'];
     $new_surname = $_POST['new_surname'];
+    $new_address = $_POST['new_address'];
+    $new_address_cd = $_POST['new_address_cd'];
+    $new_postcode = $_POST['new_postcode'];
+    $new_city = $_POST['new_city'];
     $new_email = $_POST['new_email'];
-    $new_phonenumber = $_POST['new_phonenumber'];
+    $new_phone_number = $_POST['new_phone_number'];
     $new_password = $_POST['new_password'];
     $new_password_confirm = $_POST['new_password_confirm'];
 
@@ -16,34 +20,37 @@ if (isset($_POST['new_name']) && isset($_POST['new_surname']) && isset($_POST['n
     $resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
 
     if ($resp->isSuccess()) {
-        if($new_name=='' || $new_surname=='' || $new_email=='' || $new_phonenumber=='' || $new_password=='' || $new_password_confirm==''){
+        if($new_name=='' || $new_surname=='' || $new_email=='' || $new_phone_number=='' || $new_password=='' || $new_password_confirm==''){
             $komunikat = "Pola nie mogą być puste.";
         }
         else {
-            if (!preg_match('/^[a-zA-Z0-9\-\_\.]+\@[a-zA-Z0-9\-\_\.]+\.[a-zA-Z]{2,5}$/D', $new_email)) {
-                $komunikat = "Podany adres email jest niepoprawny.";
-            }
-            else {
-                if (!preg_match('/^[0-9]{9}$/', $new_phonenumber)) {
-                    $komunikat = "Podany numer telefonu jest niepoprawny.";
-                }
-                else {
-                    if (strcmp($new_password, $new_password_confirm) !== 0) {
-                        $komunikat = "Podane hasła różnią się.";
-                    }
-                    else {
-                        if (strlen($new_password) < 5) {
-                            $komunikat = "Hasło musi mieć co najmniej 5 znaków.";
-                        }
-                        else {
-                            $new_password = password_hash($new_password, PASSWORD_DEFAULT);
-                            try {
-                                $stmt = $dbh->prepare('INSERT INTO users (id, name, surname, email, phonenumber, password, created) VALUES (null, :name, :surname, :email, :phonenumber, :password, NOW())');
-                                $stmt->execute([':name' => $new_name, ':surname' => $new_surname, ':email' => $new_email, ':phonenumber' => $new_phonenumber, ':password' => $new_password]);
-                                $komunikat = "Konto zostało zarejestrowane poprawnie.";
-                            }
-                            catch (PDOException $e) {
-                                $komunikat = "Podany adres email jest już zajęty.";
+            if (!preg_match('/^([1-9][0-9]*[\/]{0,1}[0-9]*)$/D', $new_address_cd)) {
+                $komunikat = "Podany adres jest niepoprawny.";
+            } else {
+                if (!preg_match('/^([0-9]{2}-[0-9]{3})$/D', $new_postcode)) {
+                    $komunikat = "Podany kod pocztowy jest niepoprawny.";
+                } else {
+                    if (!preg_match('/^[a-zA-Z0-9\-\_\.]+\@[a-zA-Z0-9\-\_\.]+\.[a-zA-Z]{2,5}$/D', $new_email)) {
+                        $komunikat = "Podany email jest niepoprawny.";
+                    } else {
+                        if (!preg_match('/^[0-9]{9}$/', $new_phone_number)) {
+                            $komunikat = "Podany numer telefonu jest niepoprawny.";
+                        } else {
+                            if (strcmp($new_password, $new_password_confirm) !== 0) {
+                                $komunikat = "Podane hasła różnią się.";
+                            } else {
+                                if (strlen($new_password) < 6) {
+                                    $komunikat = "Hasło musi mieć co najmniej 6 znaków.";
+                                } else {
+                                    $new_password = password_hash($new_password, PASSWORD_DEFAULT);
+                                    try {
+                                        $stmt = $dbh->prepare('INSERT INTO users (id, name, surname, address, address_cd, postcode, city, email, phone_number, password, created) VALUES (null, :name, :surname, :address, :address_cd, :postcode, :city, :email, :phone_number, :password, NOW())');
+                                        $stmt->execute([':name' => $new_name, ':surname' => $new_surname, ':address' => $new_address, ':address_cd' => $new_address_cd, ':postcode' => $new_postcode, ':city' => $new_city, ':email' => $new_email, ':phone_number' => $new_phone_number, ':password' => $new_password]);
+                                        $komunikat = "Konto zostało zarejestrowane.";
+                                    } catch (PDOException $e) {
+                                        $komunikat = "Podany adres email jest już zajęty.";
+                                    }
+                                }
                             }
                         }
                     }
