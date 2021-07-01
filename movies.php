cc3cdce6a33daf1f2508951d75041d31a50217a1
Maximiriam <dziadon.kakuzu@gmail.com>
2021-07-01 22:56:56 +0200

    halfway done with searching

--- movies.php
+++ movies.php
@@ -37,8 +37,38 @@ if (isset($_GET['show']) && intval($_GET['show']) > 0) {
         ]);
     }
     else{
-
+        // Page error
     }
+}
+
+else if(isset($_GET['search'])){
+    $title = htmlspecialchars($_GET['search'], ENT_QUOTES | ENT_HTML401);
+    $stmt = $dbh->prepare("SELECT * FROM movies WHERE title = :title");
+    $stmt -> execute([':title' => $title]);
+
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    $id = intval($row['id']);
+    $title = htmlspecialchars($row['title'], ENT_QUOTES | ENT_HTML401);
+    $short_description = htmlspecialchars($row['short_description'], ENT_QUOTES | ENT_HTML401);
+    $genre = htmlspecialchars($row['genre'], ENT_QUOTES | ENT_HTML401);
+    $duration = intval($row['duration']);
+    $production = htmlspecialchars($row['production'], ENT_QUOTES | ENT_HTML401);
+    $direction = htmlspecialchars($row['direction'], ENT_QUOTES | ENT_HTML401);
+    $poster_path = htmlspecialchars($row['poster_path'], ENT_QUOTES | ENT_HTML401);
+    $available_date = $row['available_date'];
+
+    echo $twig->render('movies.html.twig', [
+        'id' => $id,
+        'title' => $title,
+        'short_description' => $short_description,
+        'genre' => $genre,
+        'duration' => $duration,
+        'production' => $production,
+        'direction' => $direction,
+        'poster_path' => $poster_path,
+        'available_date' => $available_date
+    ]);
+
 }
 
 // Showing all the movies when id is not given
