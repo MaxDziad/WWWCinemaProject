7b7b2c63d0c32c2f6cafa76dc9880227e45919c1
KacJab <kacjab171@gmail.com>
2021-07-02 17:38:04 +0200

    działające linki na mainie, przełączanie też. mieszanie obu niebardzo

--- main.php
+++ main.php
@@ -5,16 +5,16 @@
 $stmt = $dbh->prepare("SELECT * FROM movies ORDER BY available_date DESC");
 $stmt->execute();
 
-$titles = array();
+$id = array();
 $poster_paths = array();
 $i = 0;
 
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC) and $i <=8) {
-    array_push($titles , htmlspecialchars($row['title'], ENT_QUOTES | ENT_HTML401));
+    array_push($id, $row['id']);
     array_push($poster_paths, htmlspecialchars($row['poster_path'], ENT_QUOTES | ENT_HTML401));
     $i++;
 }
 echo $twig->render('main.html.twig', [
-    'title' => $titles,
+    'id' => $id,
     'poster_path' => $poster_paths,
 ]);
