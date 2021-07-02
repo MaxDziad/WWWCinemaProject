35a8c2e3016c70e29ea481ce6175e0d9aa35005b
agunia2000 <gunia.amadeusz@gmaill.com>
2021-07-02 02:03:51 +0200

    znalazłem!

--- cart.php
+++ cart.php
@@ -1,9 +1,8 @@
 <?php
 
-echo $twig->render('cart2.html.twig', []);
-
 if (isset($_SESSION['id']) && isset($_SESSION['email'])) {
-
+    header('Location: /cart/delivery-and-payment');
 } else {
-
+    echo $twig->render('cart2.html.twig', []);
 }
+
