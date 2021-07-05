ec9d4cae8c6318803c3a592a80a3c13c015750a6
Maximiriam <dziadon.kakuzu@gmail.com>
2021-07-05 15:00:16 +0200

    insert tickets to cart

--- cart.php
+++ cart.php
@@ -8,4 +8,17 @@ if (isset($_SESSION['id']) && isset($_SESSION['email'])) {
     $next_step_cart_link = "/cart/selection";
 }
 
+if (isset($_POST['movieDate']) && isset($_POST['movieHour'])){
+    $movieDate = $_POST['movieDate'];
+    $movieHour = $_POST['movieHour'];
+    $movieTitle = $_POST['movieTitle'];
+
+    $product = array(
+        'title' => $movieTitle,
+        'date' => $movieDate,
+        'time' => $movieHour,
+    );
+    array_push($_SESSION['cart'], $product);
+}
+
 echo $twig->render('cart.html.twig', ['cart_php'=> $_SESSION['cart'], 'next_step_cart_link'=>$next_step_cart_link]);
