5e5eefeb79bcf63beab2e8252689f172d561f16e
KacJab <kacjab171@gmail.com>
2021-07-06 15:25:45 +0200

    prawie gotowe ustawienia konta

--- profile.php
+++ profile.php
@@ -73,15 +73,38 @@ if (isset($_POST['address']) && isset($_POST['address_cd']) && isset($_POST['pos
     } else {
         if (password_verify($password_verify, $user['password'])) {
 
-            if ($new_address == "" && $new_address_cd == "" && $new_postcode == "" && $new_city == "" && $new_phone_number == "") {
-                if (!preg_match('/^([1-9][0-9]*[a-zA-z]{0,1}[\/]{0,1}[0-9]*)$/D', $new_address_cd)) {
-                    $message2 = $message2."Podany adres jest niepoprawny. ";
+            if (($new_address == "" && $new_address_cd == "" && $new_postcode == "" && $new_city == "" && $new_phone_number == "") == false) {
+                if (!preg_match('/^([1-9][0-9]*[a-zA-z]{0,1}[\/]{0,1}[0-9]*)$/D', $new_address_cd) && $new_address_cd != "") {
+                    $message2 = $message2 . "Podany adres jest niepoprawny. ";
+                } elseif (!preg_match('/^([0-9]{2}-[0-9]{3})$/D', $new_postcode) && $new_postcode != "") {
+                    $message2 = $message2 . "Podany kod pocztowy jest niepoprawny.";
+                } elseif (!preg_match('/^[0-9]{9}$/', $new_phone_number) && $new_phone_number != "") {
+                    $message2 = $message2 . "Podany numer telefonu jest niepoprawny. ";
+                } else {
+
+                    if ($new_address == "") {
+                        $new_address = $user['address'];
+                    }
+                    if ($new_address_cd == "") {
+                        $new_address_cd = $user['address_cd'];
+                    }
+                    if ($new_postcode == "") {
+                        $new_postcode = $user['postcode'];
+                    }
+                    if ($new_city == ""){
+                        $new_city = $user['city'];
+                    }
+                    if ($new_phone_number == ""){
+                        $new_phone_number = $user['phone_number'];
+                    }
+
+                        $stmt = $dbh->prepare('UPDATE users SET (address = :address, address_cd = :address_cd, postcode = :postcode, city = :city, phone_number = :phone_number)  WHERE id = :id');
+                    $stmt->execute([':address' => $new_address, ':address_cd' => $new_address_cd, ':postcode' => $new_postcode, ':city' => $city, ':phone_number' => $new_phone_number, ':id' => $user['id']]);
+                    $message = "Dane zostały pomyśłnie zmienione.";
                 }
-                if (!preg_match('/^([0-9]{2}-[0-9]{3})$/D', $new_postcode)) {
-                    $message = "Podany kod pocztowy jest niepoprawny.";
-                }
-            }else{
-                $message2 = "Wybierz pole które chcesz edytować. ";
+            } else {
+                $message2 = "Edytuj jedno z pól, którego wartość chcesz zmienić. ";
+
             }
 
         } else {
@@ -91,6 +114,9 @@ if (isset($_POST['address']) && isset($_POST['address_cd']) && isset($_POST['pos
 }
 
 //przekazanie danych do twiga
+
+
+
 echo $twig->render('profile.html.twig', [
     /*data to tickets:*/
     'iterate' => $i,
@@ -99,7 +125,7 @@ echo $twig->render('profile.html.twig', [
     'time' => $time,
 
     /*data to password changing*/
-    'message' => $message,
+    'message_password' => $message,
 
     /*data to personal info changing */
     'address' => $address,
@@ -107,7 +133,7 @@ echo $twig->render('profile.html.twig', [
     'postcode' => $postcode,
     'city' => $city,
     'phone' => $phone,
-    'message2' => $message2,
+    'message_user' => $message2,
 
 ]);
 
