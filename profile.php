03054edb0191cb668eb046ec2bc6802e500f53ac
KacJab <kacjab171@gmail.com>
2021-07-07 02:14:22 +0200

    działa zmiana

--- profile.php
+++ profile.php
@@ -1,7 +1,6 @@
 <?php
 
 //Moje konto:
-$email = $_SESSION['email'];
 $stmt = $dbh->prepare("SELECT * FROM tickets WHERE email = :email ORDER BY 'date', time ");
 $stmt->execute([':email' => $_SESSION['email']]);
 $title = array();
@@ -18,8 +17,8 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 
 //Zmień hasło:
 $message = "";
-$stmt = $dbh->prepare("SELECT * FROM users WHERE email = :email");
-$stmt->execute([':email' => $_SESSION['email']]);
+$stmt = $dbh->prepare("SELECT * FROM users WHERE id = :id");
+$stmt->execute([':id' => $_SESSION['id']]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
@@ -60,9 +59,10 @@ $address_cd = $user['address_cd'];
 $postcode = $user['postcode'];
 $city = $user['city'];
 $phone_number = $user['phone_number'];
+$email = $user['email'];
 $message2 = "";
 
-if (isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['address']) && isset($_POST['address_cd']) && isset($_POST['postcode']) && isset($_POST['city']) && isset($_POST['phone-number']) && isset($_POST['email']) /*&& isset($_POST['password-verify'])*/) {
+if (isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['address']) && isset($_POST['address_cd']) && isset($_POST['postcode']) && isset($_POST['city']) && isset($_POST['phone_number']) && isset($_POST['email'])) {
 
     $new_name = $_POST['name'];
     $new_surname = $_POST['surname'];
@@ -70,61 +70,53 @@ if (isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['address']
     $new_address_cd = $_POST['address_cd'];
     $new_postcode = $_POST['postcode'];
     $new_city = $_POST['city'];
-    $new_phone_number = $_POST['phone-number'];
+    $new_phone_number = $_POST['phone_number'];
     $new_email = $_POST['email'];
 //    $password_verify = $_POST['password-verify'];
 
-//    if ($password_verify = "") {
-//        $message2 = "Musisz podać hasło";
-//    }
-//else {
-//    if (password_verify($password_verify, $user['password'])) {
 
-        if (($new_address == "" && $new_address_cd == "" && $new_postcode == "" && $new_city == "" && $new_phone_number == "") == false) {
-            if (!preg_match('/^([1-9][0-9]*[a-zA-z]{0,1}[\/]{0,1}[0-9]*)$/D', $new_address_cd) && $new_address_cd != "") {
-                $message2 = $message2 . "Podany adres jest niepoprawny. ";
-            } elseif (!preg_match('/^([0-9]{2}-[0-9]{3})$/D', $new_postcode) && $new_postcode != "") {
-                $message2 = $message2 . "Podany kod pocztowy jest niepoprawny.";
-            } elseif (!preg_match('/^[0-9]{9}$/', $new_phone_number) && $new_phone_number != "") {
-                $message2 = $message2 . "Podany numer telefonu jest niepoprawny. ";
-            } elseif (!preg_match('/^[a-zA-Z0-9\-\_\.]+\@[a-zA-Z0-9\-\_\.]+\.[a-zA-Z]{2,5}$/D', $new_email)) {
-                $message2 = $message2 . "Podany email jest niepoprawny.";
-            } else {
-
-                if ($new_address == "") {
-                    $new_address = $user['address'];
-                }
-                if ($new_address_cd == "") {
-                    $new_address_cd = $user['address_cd'];
-                }
-                if ($new_postcode == "") {
-                    $new_postcode = $user['postcode'];
-                }
-                if ($new_city == "") {
-                    $new_city = $user['city'];
-                }
-                if ($new_phone_number == "") {
-                    $new_phone_number = $user['phone_number'];
-                }
-
-                $stmt = $dbh->prepare('UPDATE users SET (address = :address, address_cd = :address_cd, postcode = :postcode, city = :city, phone_number = :phone_number)  WHERE id = :id');
-                $stmt->execute([':address' => $new_address, ':address_cd' => $new_address_cd, ':postcode' => $new_postcode, ':city' => $city, ':phone_number' => $new_phone_number, ':id' => $user['id']]);
-                $message = "Dane zostały pomyśłnie zmienione.";
-            }
+    if (($name == "" && $surname == "" && $new_address == "" && $new_address_cd == "" && $new_postcode == "" && $new_city == "" && $new_phone_number == "") == false) {
+        if (!preg_match('/^([1-9][0-9]*[a-zA-z]{0,1}[\/]{0,1}[0-9]*)$/D', $new_address_cd) && $new_address_cd != "") {
+            $message2 = $message2 . "Podany adres jest niepoprawny. ";
+        } elseif (!preg_match('/^([0-9]{2}-[0-9]{3})$/D', $new_postcode) && $new_postcode != "") {
+            $message2 = $message2 . "Podany kod pocztowy jest niepoprawny.";
+        } elseif (!preg_match('/^[0-9]{9}$/', $new_phone_number) && $new_phone_number != "") {
+            $message2 = $message2 . "Podany numer telefonu jest niepoprawny. ";
+        } elseif (!preg_match('/^[a-zA-Z0-9\-\_\.]+\@[a-zA-Z0-9\-\_\.]+\.[a-zA-Z]{2,5}$/D', $new_email)) {
+            $message2 = $message2 . "Podany email jest niepoprawny.";
         } else {
-            $message2 = "Edytuj jedno z pól, którego wartość chcesz zmienić. ";
 
+            $stmt = $dbh->prepare('UPDATE users SET name = :name, surname = :surname, address = :address, address_cd = :address_cd, postcode = :postcode, city = :city, phone_number = :phone_number, email = :email WHERE id = :id');
+            $stmt->execute([':name' => $new_name, ':surname' => $new_surname, ':address' => $new_address, ':address_cd' => $new_address_cd, ':postcode' => $new_postcode, ':city' => $new_city, ':phone_number' => $new_phone_number, ':email' => $new_email, ':id' => $user['id']]);
+            $message2 = "Dane zostały pomyślnie zmienione.";
         }
+    } else {
+        $message2 = "Edytuj jedno z pól, którego wartość chcesz zmienić. ";
+
+    }
 
 //    } else {
 //        $message2 = "Hasło jest niepoprawne. ";
 //    }
 //    }
 }
+$message3="";
 
+if (isset($_POST['password_confirm'])) {
+    $password_confirm = $_POST['password_confirm'];
+
+    if ($password_confirm = "") {
+        $message3 = "Musisz podać hasło.";
+    }else{
+        if(password_verify($password_confirm,$user['password'])){
+            $stmt = $dbh->prepare('DELETE FROM users WHERE id = :id');
+            $stmt ->execute([':id' => $user['id']]);
+        }else{
+            $message3 = "Hasło jest błędne.";
+        }
+    }
+}
 //przekazanie danych do twiga
-
-
 echo $twig->render('profile.html.twig', [
     /*data to tickets:*/
     'iterate' => $i,
@@ -146,6 +138,8 @@ echo $twig->render('profile.html.twig', [
     'email' => $email,
     'message_user' => $message2,
 
-]);
+    /*data to deleting an account*/
+    'message_delete' => $message3,
 
+]);
 
