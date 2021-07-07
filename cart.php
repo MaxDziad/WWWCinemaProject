b40bfdf418b3d93c162a01019da9acba2674de55
Maximiriam <dziadon.kakuzu@gmail.com>
2021-07-07 14:01:33 +0200

    number of tickets restriction

--- cart.php
+++ cart.php
@@ -7,18 +7,40 @@ if (isset($_SESSION['id']) && isset($_SESSION['email'])) {
 }
 
 if (isset($_POST['movieDate']) && isset($_POST['movieHour'])){
+
     $movieTitle = $_POST['movieTitle'];
     $movieDate = $_POST['movieDate'];
     $movieHour = $_POST['movieHour'];
 
-    $ticket = array(
-        'title' => $movieTitle,
-        'date' => $movieDate,
-        'time' => $movieHour,
-        'price' => 19.99
-    );
-    array_push($_SESSION['cart'], $ticket);
-    header('Location: /cart');
+    $stmt = $dbh->prepare("SELECT * FROM tickets WHERE
+                            time = :time AND date = :date AND title = :title");
+    $stmt->execute([':title' => $movieTitle, ':time' => $movieHour, ':date' => $movieDate]);
+
+    // count similar tickets in cart
+    $similarTicketsInCart = 0;
+    foreach($_SESSION['cart'] as $ticket){
+        if($ticket['title'] == $movieTitle && $ticket['date'] == $movieDate && $ticket['time'] == $movieHour){
+            $similarTicketsInCart ++;
+        }
+    }
+
+    // check if tickets are available
+    if($stmt->rowCount() + $similarTicketsInCart >= 2) {
+        $message = "Nie można dodać więcej biletów.";
+        echo "<script type='text/javascript'>alert('$message');</script>";
+    }
+
+    else {
+        // add ticket to cart
+        $ticket = array(
+            'title' => $movieTitle,
+            'date' => $movieDate,
+            'time' => $movieHour,
+            'price' => 19.99
+        );
+        array_push($_SESSION['cart'], $ticket);
+        header('Location: /cart');
+    }
 }
 
 if (isset($_GET['remove']) && intval($_GET['remove']) >= 0) {
